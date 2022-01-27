71cee3b97a74a68f0481b38db4071fb92a5fcb41
LUKAS SANTOS VENANCIO <21276375@cfp127aluno.local>
2022-01-27 16:46:46 -0300

    Exercise completed

---
+++ calculadora_simples.php
@@ -0,0 +1,78 @@
+<?php
+$valor1 = (double) 0;
+$valor2 = (double) 0;
+$result = (double) 0;
+
+if(isset($_POST['btncalc']))
+{
+	$valor1 = $_POST['txtn1'];
+	$valor2 = $_POST['txtn2'];
+	$rdoOperacao = $_POST['rdocalc'];
+
+	if($_POST['txtn1'] == "" || $_POST['txtn2'] == "")
+	{
+		echo('<p class="msgErro">Preencha todos os campos!</p>');
+	}else
+	{
+		if(!is_numeric($valor1) || !is_numeric($valor2))
+		{
+			echo('<p class="msgErro">Insira apenas números!</p>');
+		}else
+		{
+			if(strchr($rdoOperacao, 'somar'))
+			{
+				$result = $valor1 + $valor2;
+
+			}else if (strchr($rdoOperacao, 'subtrair'))
+			{
+				$result = $valor1 - $valor2;
+			}else if (strchr($rdoOperacao, 'multiplicar'))
+			{
+				$result = $valor1 * $valor2;
+			}else
+			{
+				$result = $valor1 / $valor2;
+			}
+		}
+	}
+}
+?>
+<html>
+    <head>
+        <title>Calculadora - Simples</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+	<body>
+        
+        <div id="conteudo">
+            <div id="titulo">
+                Calculadora Simples
+            </div>
+
+            <div id="form">
+                <form name="frmcalculadora" method="post" action="">
+						Valor 1:<input type="text" name="txtn1" value="<?php echo($valor1)?>" > <br>
+						Valor 2:<input type="text" name="txtn2" value="<?php echo($valor2)?>" > <br>
+						<div id="container_opcoes">
+							<input type="radio" name="rdocalc" value="somar" checked>Somar <br>
+							<input type="radio" name="rdocalc" value="subtrair" >Subtrair <br>
+							<input type="radio" name="rdocalc" value="multiplicar" >Multiplicar <br>
+							<input type="radio" name="rdocalc" value="dividir" >Dividir <br>
+							
+							<input type="submit" name="btncalc" value ="Calcular" >
+							
+						</div>	
+						<div id="resultado">
+						<?php echo($result)?>
+						</div>
+						
+					</form>
+            </div>
+           
+        </div>
+        
+		
+	</body>
+
+</html>
+
